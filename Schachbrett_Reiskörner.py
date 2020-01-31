b9a714166f243989fcace9640a4087d9f8dcf936
Frey <Stefan.Frey@adesso.de>
2020-01-31 13:19:06 +0100

    change2

--- Schachbrett_Reiskörner.py
+++ Schachbrett_Reiskörner.py
@@ -1,13 +1,20 @@
+import matplotlib.pyplot as plt
 summe = 0
+feldListe = []
+
 
 for feld in range(64):
     reiskorn = 2**feld
-    summe = summe + reiskorn
-    print("Feld {}. = {:>30,} Reiskörner und damit insgesamt \
-            {:>30,} Reiskörner" \
-        .format(feld+1,reiskorn,summe))
-
+    feldListe.append(reiskorn)
+    summe += reiskorn
+    print(f"Feld {feld+1}. = {reiskorn:>30,} Reiskörner und damit insgesamt \
+            {summe:>30,} Reiskörner") 
+        
 gewicht = summe * 0.02 / 1000000
 print()
-print("Wenn ein Reiskorn 0,02 Gramm wieg, wiegen die gesamten"\
-    "Reiskörner {:,.0f} Tonnen".format(gewicht))
+print(f"Wenn ein Reiskorn 0,02 Gramm wieg, wiegen die gesamten"\
+    f"Reiskörner {gewicht:,.0f} Tonnen")
+
+
+plt.plot(feldListe)
+plt.show()
